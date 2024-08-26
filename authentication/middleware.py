f751716847e36013c4b0534255c2cae26385dfed
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-08-26 20:24:46 +0000

    fix : avoid cheking authorisation bearer

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -22,28 +22,16 @@ def get_user(token_key):
 
 class TokenAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        token_key = None
-        if b'authorization' in headers:
-            try:
-                scheme, token_key = headers[b'authorization'].decode().split()
-                if scheme.lower() != 'bearer':
-                    raise ValueError("Invalid authorization scheme")
-            except (ValueError, IndexError):
-                # Log the invalid token or scheme
-                scope['user'] = AnonymousUser()
-                return await self.inner(scope, receive, send)
-       
         query_string = parse_qs(scope['query_string'].decode())
         token_key = query_string.get('token', [None])[0]
         print('token_key', token_key)
-        if token_key is None:
+        if token_key == None:
             raise ValueError("No token provided")
         else:
             user = await get_user(token_key)
             scope['user'] = user
             if user is AnonymousUser():
                 raise ValueError("Invalid token")
-
+        print('user', scope['user'])
         return await super().__call__(scope, receive, send)
 
